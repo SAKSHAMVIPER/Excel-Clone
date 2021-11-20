b5ff71509593627ab8bf5723d21b6bca76168183
SAKSHAMVIPER <sakshamsinghrkt387@gmail.com>
2021-11-20 10:56:15 +0530

    first commit

---
+++ gridScript.js
@@ -0,0 +1,137 @@
+let toprow = document.querySelector(".top_row");
+
+for(let i=0;i<26;i++){
+    let div = document.createElement("div");
+    div.textContent=String.fromCharCode(65+i);
+    div.setAttribute("class","cell");
+   
+    toprow.appendChild(div);
+}
+
+
+let leftcol = document.querySelector(".left_col")
+for(let i=1;i<=200;i++){
+    let div = document.createElement("div");
+    div.textContent=i;
+    div.setAttribute("class","cell");
+    leftcol.appendChild(div);
+}
+
+
+let grid = document.querySelector(".grid");
+for(let i=0;i<200;i++){
+    let row = document.createElement("div");
+    row.setAttribute("class","row")
+    
+    for(let j=0;j<26;j++){
+        let div = document.createElement("div");
+        div.setAttribute("contentEditable",true);
+        // div.textContent=String.fromCharCode(65+j)+i;
+        //every cell identification is required
+        div.setAttribute("rId",i);
+        div.setAttribute("cId",j);
+        div.setAttribute("class","cell");
+       
+        row.appendChild(div);
+
+    }
+    grid.appendChild(row)
+}
+
+
+//if a click on any cell
+let allGridCells = document.querySelectorAll(".grid .cell");
+let addressinput = document.querySelector(".address_input");
+for(let j=0;j<allGridCells.length;j++){
+    allGridCells[j].addEventListener("click",function(){
+        // alert("hello");
+let prevAddresss
+ = addressinput.value;
+if(prevAddresss != ""){
+let ridcidobj = getRidCidfromaddress(prevAddresss);
+ let prevCell= document.querySelector(`.grid .cell[rid='${ridcidobj.rid}'][cid='${ridcidobj.cid}']`);
+ prevCell.style.border ="0.9px solid gray";
+            //prevCell.style.borderRight="none";
+            prevCell.style.borderTop="none";
+            prevCell.style.borderLeft="none";
+
+}
+//then i will get the address
+        let rid = allGridCells[j].getAttribute("rid");
+        let cid = allGridCells[j].getAttribute("cid");
+        rid = Number(rid);
+        cid = Number(cid);
+        // alert(rid + " "+ cid)
+        
+        addressinput.value = String.fromCharCode(cid+65) + (rid+1);
+
+        let cCell= document.querySelector(`.grid .cell[rid='${rid}'][cid='${cid}']`);
+        cCell.style.border = "2px solid #1B9CFC";
+   
+        let cellObject = db[rid][cid];
+        // font size 
+        let fontSize = cellObject.fontSize;
+        fontsizeinput.value = fontSize;
+       //boldness
+       boldIcon.classList.remove("selected");
+       if(cellObject.bold){
+        boldIcon.classList.add("selected");
+       }
+       //italic
+
+       italicIcon.classList.remove("selected");
+       if(cellObject.italic){
+        italicIcon.classList.add("selected");
+       }
+
+
+       //underline
+       underlineIcon.classList.remove("selected");
+       if(cellObject.underline){
+        underlineIcon.classList.add("selected");
+       }
+
+    })
+    }
+
+
+
+
+//for first cell
+let firstCell = document.querySelector(".grid .cell[rid='0'][cid='0']");
+firstCell.click();
+firstCell.focus();
+// firstCell.style.border = "2px solid green";
+
+function getRidCidfromaddress(address){
+    let Asciivalue = address.charCodeAt(0);
+    let cid= Asciivalue - 65;
+let rid =Number(address.substring(1))-1;
+    return {
+    rid:rid,cid:cid
+   
+}
+}
+
+
+
+//2way binding*******************************
+//default value for every cell
+let db = [];
+for(let i=0;i<200;i++){
+    let rowArr = [];
+    for(let j=0;j<26;j++){
+        let cellObject = {
+            color:"black",
+            backgroundColor: "white",
+            fontFamily: "san-serif",
+            fontSize: 14,
+            halign: "center",
+            italic: false,
+            underline: false,
+            bold: false
+        }
+        rowArr.push(cellObject);
+    }
+    db.push( rowArr);
+}
---
+++ menuScript.js
@@ -0,0 +1,151 @@
+ let fontfamilyinput = document.querySelector(".font_family_input");
+ let fontsizeinput = document.querySelector(".font_size_input");
+ let boldIcon = document.querySelector(".fa-bold");
+ let italicIcon = document.querySelector(".fa-italic");
+ let underlineIcon = document.querySelector(".fa-underline");
+ let allignmentContainer = document.querySelector(".allignment_container");
+ let textColorHinput = document.querySelector(".text_color");
+ let textColorinput = document.querySelector(".fa-tint");
+ let backgroundHinput = document.querySelector(".background_color");
+ let backgroundinput = document.querySelector(".fa-fill-drip");
+
+ 
+ fontsizeinput.addEventListener("change",function(){
+     let fontSize = fontsizeinput.value;
+    let address = addressinput.value;
+    let ridcidobj = getRidCidfromaddress(address);
+    let tobechangedCell= document.querySelector(`.grid .cell[rid='${ridcidobj.rid}'][cid='${ridcidobj.cid}']`);
+    tobechangedCell.style.fontSize = fontSize+"px";
+
+
+    let { rid, cid} = getRidCidfromaddress(address);
+   let cellObject = db[rid][cid]
+   cellObject.fontSize = fontSize;
+   
+    
+    })
+
+
+    fontfamilyinput.addEventListener("change",function(){
+        let fontFamily = fontfamilyinput.value;
+        let address = addressinput.value;
+        let ridcidobj = getRidCidfromaddress(address);
+        let tobechangedCell= document.querySelector(`.grid .cell[rid='${ridcidobj.rid}'][cid='${ridcidobj.cid}']`);
+        tobechangedCell.style.fontFamily  = fontFamily;
+
+        let { rid, cid} = getRidCidfromaddress(address);
+        db[rid][cid].fontFamily = fontFamily;
+    })   
+
+
+
+    boldIcon.addEventListener("click",function(){
+        let address = addressinput.value;
+        let ridcidobj = getRidCidfromaddress(address);
+        let tobechangedCell= document.querySelector(`.grid .cell[rid='${ridcidobj.rid}'][cid='${ridcidobj.cid}']`);
+        let cellObject = db[ridcidobj.rid][ridcidobj.cid];
+        if(cellObject.bold){
+            tobechangedCell.style.fontWeight  = "normal";
+            boldIcon.classList.remove("selected");
+            cellObject.bold = false;
+        }else{
+            tobechangedCell.style.fontWeight  = "bold";
+            boldIcon.classList.add("selected");
+            cellObject.bold = true;
+        }  
+       
+    })
+
+
+
+    italicIcon.addEventListener("click",function(){
+        let address = addressinput.value;
+        let ridcidobj = getRidCidfromaddress(address);
+        let tobechangedCell= document.querySelector(`.grid .cell[rid='${ridcidobj.rid}'][cid='${ridcidobj.cid}']`);
+       
+        let cellObject = db[ridcidobj.rid][ridcidobj.cid];
+        if(cellObject.italic){
+            tobechangedCell.style.fontStyle  = "none";
+            italicIcon.classList.remove("selected");
+                cellObject.italic = false;
+        }else{
+            tobechangedCell.style.fontStyle  = "italic";
+        italicIcon.classList.add("selected");
+            cellObject.italic = true;
+        }  
+       
+        
+
+    })
+
+
+
+    underlineIcon.addEventListener("click",function(){
+        let address = addressinput.value;
+        let ridcidobj = getRidCidfromaddress(address);
+        let tobechangedCell= document.querySelector(`.grid .cell[rid='${ridcidobj.rid}'][cid='${ridcidobj.cid}']`);
+       
+        let cellObject = db[ridcidobj.rid][ridcidobj.cid];
+        if(cellObject.underline){
+            tobechangedCell.style.textDecoration = "none";
+            underlineIcon.classList.remove("selected");
+            cellObject.underline = false;
+        }else{
+            tobechangedCell.style.textDecoration = "underline";
+            underlineIcon.classList.add("selected");
+            cellObject.underline = true;
+        }  
+       
+        
+
+    })
+
+
+    allignmentContainer.addEventListener("click",function(e){
+        if(e.target !== allignmentContainer){
+              let classArr = e.target.classList;
+              let hAlignment = classArr[classArr.length-1];
+              let address = addressinput.value;
+              let ridcidobj = getRidCidfromaddress(address);
+              let tobechangedCell= document.querySelector(`.grid .cell[rid='${ridcidobj.rid}'][cid='${ridcidobj.cid}']`);
+              tobechangedCell.style.textAlignt = hAlignment;
+              e.target.classList.add("selected");
+        }
+    })
+
+    textColorinput.addEventListener("click",function(e){
+        textColorHinput.click();
+      
+        
+    })
+
+
+    textColorHinput.addEventListener("change",function(e){
+        let color = textColorHinput.value;
+        let address = addressinput.value;
+        let ridcidobj = getRidCidfromaddress(address);
+        let tobechangedCell= document.querySelector(`.grid .cell[rid='${ridcidobj.rid}'][cid='${ridcidobj.cid}']`);
+        tobechangedCell.style.color = color;
+
+
+        let { rid, cid} = getRidCidfromaddress(address);
+        db[rid][cid].color = color;
+       
+    })
+
+
+    backgroundinput.addEventListener("click",function(e){
+        backgroundHinput.click();
+    })
+
+
+    backgroundHinput.addEventListener("change",function(e){
+        let color = backgroundHinput.value;
+        let address = addressinput.value;
+        let ridcidobj = getRidCidfromaddress(address);
+        let tobechangedCell= document.querySelector(`.grid .cell[rid='${ridcidobj.rid}'][cid='${ridcidobj.cid}']`);
+        tobechangedCell.style.backgroundColor = color;
+        let { rid, cid} = getRidCidfromaddress(address);
+        db[rid][cid].backgroundColor = color;
+       
+    })
